bded4e0340d6528a632a8b615634a9622fc54a03
lumilitas <lucasmengineer@edu.unifor.br>
2022-02-02 09:32:52 -0300

    Instalada as fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import AppLoading from 'expo-app-loading';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular
+  });
+
+  if (!fontsLoaded) {
+    return < AppLoading/>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
