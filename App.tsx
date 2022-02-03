61fa97bdd98e253314ec3d26833ca0f10d613e7f
lumilitas <lucasmengineer@edu.unifor.br>
2022-02-03 10:57:56 -0300

    Implementada a estrutura do hook de autenticação

--- App.tsx
+++ App.tsx
@@ -6,8 +6,11 @@ import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 
 import { ThemeProvider } from 'styled-components/native';
+
+
 import theme from './src/theme';
 import { SignIn } from '@screens/SignIn';
+import { AuthProvider } from '@hooks/auth';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,7 +25,10 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style='light' translucent backgroundColor='transparent'/>
-      <SignIn />
+
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
     </ThemeProvider>
   );
 }
