4ccc833c0097c99cd4cf26e4449b28fce0af3807
lumilitas <lucasmengineer@edu.unifor.br>
2022-02-03 10:00:12 -0300

    Implementado o componente input

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,10 +1,22 @@
+import { Input } from "@components/Input";
 import React from "react";
 import { Container } from "./styles";
 
 export function SignIn() {
   return(
     <Container>
-      
+      <Input
+        placeholder="E-mail"
+        type="secondary"
+        autoCorrect={false}
+        autoCapitalize="none"
+      />
+
+      <Input
+        placeholder="Senha"
+        type="secondary"
+        secureTextEntry
+      />
     </Container>
   )
 }
