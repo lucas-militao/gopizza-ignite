34bb3768b68c4fb8736c0a881e5c5ea818d56cec
lumilitas <lucasmengineer@edu.unifor.br>
2022-02-03 10:45:52 -0300

    Finalizada a tela de SignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,29 +1,54 @@
+import React from "react";
+import { KeyboardAvoidingView, Platform } from "react-native";
+
+import brandImg from "@assets/brand.png";
+
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
-import React from "react";
-import { Container } from "./styles";
+
+import { 
+  Brand, 
+  Container, 
+  Content, 
+  ForgotPasswordButton, 
+  ForgotPasswordLabel, 
+  Title } 
+from "./styles";
 
 export function SignIn() {
   return(
     <Container>
-      <Input
-        placeholder="E-mail"
-        type="secondary"
-        autoCorrect={false}
-        autoCapitalize="none"
-      />
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+        <Content>
 
-      <Input
-        placeholder="Senha"
-        type="secondary"
-        secureTextEntry
-      />
+          <Brand source={brandImg}/>
 
-      <Button
-        title="Entrar"
-        type="secondary"
-        isLoading={true}
-      />
+          <Title>Login</Title>
+
+          <Input
+            placeholder="E-mail"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+
+          <Input
+            placeholder="Senha"
+            type="secondary"
+            secureTextEntry
+          />
+
+          <ForgotPasswordButton>
+            <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
+          </ForgotPasswordButton>
+          
+          <Button
+            title="Entrar"
+            type="secondary"
+          />
+
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   )
 }
