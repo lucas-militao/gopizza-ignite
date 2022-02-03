459dc178d9200d0e9c3931affa4e9659a613ecab
lumilitas <lucasmengineer@edu.unifor.br>
2022-02-03 09:41:43 -0300

    Implementado o background do signIn

--- App.tsx
+++ App.tsx
@@ -5,8 +5,8 @@ import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 
 import { ThemeProvider } from 'styled-components/native';
-import theme from '@src/theme';
-import { SignIn } from '@src/screens/SignIn';
+import theme from './src/theme';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
