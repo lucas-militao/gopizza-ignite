85a1ee9c75b7a5b68f385da56e05d768340f180d
lumilitas <lucasmengineer@edu.unifor.br>
2022-02-07 13:48:22 -0300

    Implementado o button back

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,3 +1,4 @@
+import { ButtonBack } from "@components/ButtonBack";
 import React from "react";
 import { Platform, TouchableOpacity } from "react-native";
 import { Container, Header, Title, DeleteLabel } from "./styles";
@@ -6,6 +7,8 @@ export function Product() {
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
       <Header>
+        <ButtonBack />
+
         <Title>Cadastrar</Title>
 
         <TouchableOpacity>
