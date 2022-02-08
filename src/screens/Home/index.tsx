7cc77768fc49740bbdba51948d4c9e5a9d7eea92
lumilitas <lucasmengineer@edu.unifor.br>
2022-02-08 10:25:44 -0300

    Implementado o header de Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,29 @@
 import React from "react";
-import { Container } from "./styles";
+import { Container, Greeting, GreetingEmoji, GreetingText, Header } from "./styles";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import happyEmoji from '@assets/happy.png';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { useTheme } from "styled-components/native";
+import { TouchableOpacity } from "react-native";
 
 export function Home() {
+  const { COLORS } = useTheme();
+
   return(
     <Container>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Header>
+          <Greeting>
+            <GreetingEmoji source={happyEmoji}/>
+            <GreetingText>Olá, Admin</GreetingText>
+          </Greeting>
 
+          <TouchableOpacity>
+            <MaterialIcons name="logout" color={ COLORS.TITLE } size={24}/>
+          </TouchableOpacity>
+        </Header>
+      </GestureHandlerRootView>
     </Container>
   )
 }
