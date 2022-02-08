e4e43fe44778fd3816c9065c5102df928ff76675
lumilitas <lucasmengineer@edu.unifor.br>
2022-02-08 14:32:15 -0300

    Implementado método de busca de pizzas cadastradas

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Greeting, GreetingEmoji, GreetingText, Header, MenuHeader, MenuItemsNumber, Title } from "./styles";
 import { MaterialIcons } from "@expo/vector-icons";
 
 import happyEmoji from '@assets/happy.png';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { useTheme } from "styled-components/native";
-import { TouchableOpacity } from "react-native";
+import { Alert, TouchableOpacity } from "react-native";
 import { SearchBar } from "@components/SearchBar";
-import { ProductCard } from "@components/ProductCard";
+import { ProductCard, ProductProps } from "@components/ProductCard";
+import firestore from '@react-native-firebase/firestore';
 
 export function Home() {
   const { COLORS } = useTheme();
+
+  function fetchPizza(value: string) {
+    const formattedValue = value.toLocaleLowerCase().trim();
+
+    firestore()
+      .collection('pizzas')
+      .orderBy('name_insensitive')
+      .startAt(formattedValue)
+      .endAt(`${formattedValue}\uf8ff`)
+      .get()
+      .then(response => {
+        const data = response.docs.map(doc => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          }
+        }) as ProductProps[];
+
+        console.log(data);
+      })
+      .catch(() => Alert.alert('Consulta', 'Não foi possível realizar a consulta!'));
+  }
+
+  useEffect(() => {
+    fetchPizza('');
+  }, []);
 
   return(
     <Container>
