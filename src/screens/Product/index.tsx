0249579175ba74f80d5a5ec8b08d448569e9f605
lumilitas <lucasmengineer@edu.unifor.br>
2022-02-08 09:32:15 -0300

    Implementado o formulário de produto

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { ButtonBack } from "@components/ButtonBack";
 import { Photo } from "@components/Photo";
-import { Platform, TouchableOpacity } from "react-native";
+import { Platform, ScrollView, TouchableOpacity } from "react-native";
 import * as ImagePicker from 'expo-image-picker';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { Container, Header, Title, DeleteLabel, Upload, PickImageButton } from "./styles";
+import { Container, Header, Title, DeleteLabel, Upload, PickImageButton, Form, InputGroup, Label, InputGroupHeader, MaxCharacters } from "./styles";
 import { InputPrice } from "@components/InputPrice";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 export function Product() {
   const [image, setImage] = useState('');
@@ -27,31 +29,63 @@ export function Product() {
 
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <Header>
-          <ButtonBack />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <Header>
+            <ButtonBack />
 
-          <Title>Cadastrar</Title>
+            <Title>Cadastrar</Title>
 
-          <TouchableOpacity>
-            <DeleteLabel>Deletar</DeleteLabel>
-          </TouchableOpacity>
-        </Header>
+            <TouchableOpacity>
+              <DeleteLabel>Deletar</DeleteLabel>
+            </TouchableOpacity>
+          </Header>
 
-        <Upload>
-          <Photo uri={image}/>
+          <Upload>
+            <Photo uri={image}/>
 
-          <PickImageButton 
-            title="Carregar" 
-            type="secondary" 
-            onPress={handleImagePicker}
-          />
-        </Upload>
+            <PickImageButton 
+              title="Carregar" 
+              type="secondary" 
+              onPress={handleImagePicker}
+            />
+          </Upload>
 
-        <InputPrice size="P"/>
-        <InputPrice size="M"/>
-        <InputPrice size="G"/>
-      </GestureHandlerRootView>
+          <Form>
+            <InputGroup>
+              <Label>Nome</Label>
+              <Input />
+            </InputGroup>
+
+            <InputGroup>
+              <InputGroupHeader>
+                <Label>Descrição</Label>
+                <MaxCharacters>0 de 60 caracteres</MaxCharacters>
+              </InputGroupHeader>
+
+              <Input
+                multiline
+                maxLength={60}
+                style={{ height: 80 }}
+              />
+            </InputGroup>
+
+            <InputGroup>
+              <Label>Tamanhos e preços</Label>
+
+              <InputPrice size="P"/>
+              <InputPrice size="M"/>
+              <InputPrice size="G"/>
+            </InputGroup>
+
+            <Button
+              title="Cadastrar pizza"
+            />
+
+          </Form>
+        </GestureHandlerRootView>
+
+      </ScrollView>
     </Container>
   )
 }
