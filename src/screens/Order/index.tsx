6e2f94f50c1b39aca3d950c19fec7825e47dd030
lumilitas <lucasmengineer@edu.unifor.br>
2022-02-11 06:50:56 -0300

    Implementado o formulário de pedido

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -3,34 +3,64 @@ import { RadionButton } from "@components/RadioButton";
 import React, { useState } from "react";
 import { Platform } from "react-native";
 import { PIZZA_TYPES } from "@utils/pizzaTypes";
-import { Container, Header, Photo, Sizes } from "./styles";
+import { Container, ContentScroll, Form, FormRow, Header, InputGroup, Label, Photo, Price, Sizes, Title } from "./styles";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export function Order() {
   const [size, setSize] = useState('');
 
   return(
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-      <Header>
-        <ButtonBack
-          onPress={() => {}}
-          style={{ marginBottom: 108 }}
-        />
-      </Header>
-      
-      <Photo source={{ uri: 'http://github.com/lucas-militao.png' }}/>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <ContentScroll>
 
-      <Sizes>
-        {
-          PIZZA_TYPES.map(item => (
-            <RadionButton
-              key={item.id}
-              title={item.name}
-              selected={size === item.id}
-              onPress={() => setSize(item.id)}
+          <Header>
+            <ButtonBack
+              onPress={() => {}}
+              style={{ marginBottom: 108 }}
             />
-          ))
-        }
-      </Sizes>
+          </Header>
+          
+          <Photo source={{ uri: 'http://github.com/lucas-militao.png' }}/>
+
+          <Form>
+            <Title>Nome da pizza</Title>
+            <Label>Selecione um tamanho</Label>
+            <Sizes>
+              {
+                PIZZA_TYPES.map(item => (
+                  <RadionButton
+                    key={item.id}
+                    title={item.name}
+                    selected={size === item.id}
+                    onPress={() => setSize(item.id)}
+                  />
+                ))
+              }
+            </Sizes>
+
+            <FormRow>
+              <InputGroup>
+                <Label>Número da mesa</Label>
+                <Input keyboardType="numeric"/>
+              </InputGroup>
+
+              <InputGroup>
+                <Label>Quantidade</Label>
+                <Input keyboardType="numeric"/>
+              </InputGroup>
+            </FormRow>
+
+            <Price>Valor de R$ 00,00</Price>
+
+            <Button
+              title="Confirmar pedido"
+            />
+          </Form>
+        </ContentScroll>
+      </GestureHandlerRootView>
     </Container>
   )
 }
